cfbd7f25b13c81fa0c92cb6144ac311594c5baae
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-03 17:37:41 +0530

    Add AppBar with search functionality to HomePage; improve layout and user interaction by including a search icon.

--- weather_app/lib/home_page.dart
+++ weather_app/lib/home_page.dart
@@ -5,6 +5,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Home Page')));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Location'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: IconButton(
+              icon: const Icon(Icons.search, size: 30, color: Colors.black),
+              onPressed: () {
+                // TODO: Implement search functionality
+                print('Search icon pressed');
+              },
+              tooltip: 'Search',
+            ),
+          ),
+        ],
+      ),
+
+      body: Center(child: Text('Home Page')),
+    );
   }
 }
